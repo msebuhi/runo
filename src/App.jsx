5cbe4a5d58c0a2d288b8b3b78103df8ce9bf10f1
sebuhi <sebuhimmmdov189@gmail.com>
2023-12-18 13:41:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./assets/img/logo.png";
+function App() {
+  return (
+    <>
+      <header>
+        <div><img src="" alt="" /></div>
+      </header>
+    </>
+  );
+}
+
+export default App;
